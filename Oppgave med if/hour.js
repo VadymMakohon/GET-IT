a6a5b41ef722d80f987b42e14f886ef2900e5e26
VadymMakohon <errorks7@gmail.com>
2024-08-23 11:29:18 +0200

    added first code with if

---
+++ Oppgave med if/hour.js
@@ -0,0 +1,15 @@
+
+// Hour
+// If hour is between 6am and 12pm: Good morning!
+// If it is between 12pm and 6pm: Good afternoon!
+// otherwise: good evening!
+
+let hour = 10;
+
+if (hour >= 6 && hour < 12) {
+    console.log('Good morning!');
+} else if (hour >= 12 && hour < 18) {
+    console.log('Good afternoon!');
+} else {
+    console.log('Good evening!');
+}
