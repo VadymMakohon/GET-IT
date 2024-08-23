f3fed5431d56ad072687d09594ab27477470b0ee
VadymMakohon <errorks7@gmail.com>
2024-08-23 16:55:35 +0200

    another if training file

---
+++ Oppgaver med if/time.js
@@ -0,0 +1,11 @@
+// IF STATEMENTS = if a condition is true, execute some code
+//                 if not, do somethhing else
+
+let time = 9;
+
+if (time < 12) {
+    console.log("Good morning!");
+}
+else {
+    console.log("Good afternoon!");
+}
