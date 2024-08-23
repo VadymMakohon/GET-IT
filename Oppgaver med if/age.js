8a35edbe68ce508443210172afff07858a525f34
VadymMakohon <errorks7@gmail.com>
2024-08-23 16:50:02 +0200

    another if training file

---
+++ Oppgaver med if/age.js
@@ -0,0 +1,11 @@
+// IF STATEMENTS = if a condition is true, execute some code
+//                 if not, do somethhing else
+
+let age = 13;
+
+if (age >= 18) {
+    console.log("You are old enough to enter this site");
+}
+else {
+    console.log("You must be 18+ to enter this site")
+}
