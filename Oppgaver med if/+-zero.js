64fcb93156525bae85581ac3169f0bcdeb6e077b
VadymMakohon <errorks7@gmail.com>
2024-08-24 13:39:33 +0200

    working with 'if' exercises

---
+++ Oppgaver med if/+-zero.js
@@ -0,0 +1,12 @@
+
+let num = 0;
+
+if (num > 0) {
+    console.log('Positive')
+}
+else if (num < 0) {                  // remember to use "else if" not jut "if" here
+    console.log('Negative')
+}
+else {
+    console.log('Zero')
+}
