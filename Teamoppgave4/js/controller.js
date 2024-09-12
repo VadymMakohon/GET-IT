fbf69607dcdef7d9802f3cf5f60c87d3428c97b8
VadymMakohon <errorks7@gmail.com>
2024-09-12 10:59:41 +0200

    updated error message

--- Teamoppgave4/js/controller.js
+++ Teamoppgave4/js/controller.js
@@ -1,11 +1,19 @@
+/* 
+    1. Sjekke om du har nok penger (og få opp feilmelding hvis ikke) OK
+    2. Sjekke at det er cola i maskinen (få opp feilmelding hvis ikke) OK
+    3. Hvis du betaler for mye skal du få riktig veksel tilbake OK
+    4. Cola skal vises i utkast (utenfor blå boks) OK
+    5. Cola i maskin skal minke OK
+    6. Myntene du har betalt for cola skal inn i maskinen (coinsInMachine) OK
+*/
 function buyCoke() {
-    const valueOfCoinsInserted = valueFromCoinCounts(coinsInserted);
+    const valueOfInsertedCoins = valueFromCoinCounts(coinsInserted);
 
-    if (!checksPassed(valueOfCoinsInserted)) return;
+    if (!inStockAndEnoughInserted(valueOfInsertedCoins)) return;
 
     takeInsertedCoins();
 
-    returnChange(valueOfCoinsInserted - cokePrice);
+    returnChange(valueOfInsertedCoins - cokePrice);
 
     cokesInStore--;
     isCokeInDelivery = true;
@@ -21,25 +29,28 @@ function insertCoin(coinIndex) {
 function returnCoins() {
     coinsReturned = [...coinsInserted];
     coinsInserted = [0, 0, 0, 0];
+    errorMessage = "";
     updateView();
 }
 
 function takeCoins() {
     coinsReturned = [0, 0, 0, 0];
+    errorMessage = "";
     updateView();
 }
 
 function takeCoke() {
     isCokeInDelivery = false;
+    errorMessage = "";
     updateView();
 }
 
 function returnChange(change) {
-    for (let j = 3; j >= 0; j--) {
-        let coinValue = coinValueFromIndex(j);
-        while (change - coinValue >= 0 && coinsInMachine[j] > 0) {
-            coinsReturned[j]++;
-            coinsInMachine[j]--;
+    for (let i = 3; i >= 0; i--) {
+        let coinValue = coinValueFromIndex(i);
+        while (change - coinValue >= 0 && coinsInMachine[i] > 0) {
+            coinsReturned[i]++;
+            coinsInMachine[i]--;
             change -= coinValue;
         }
     }
@@ -52,34 +63,17 @@ function takeInsertedCoins() {
     coinsInserted = [0, 0, 0, 0];
 }
 
-function checksPassed(valueOfCoinsInserted) {
-    if (!inStock() || !hasInsertedEnough(valueOfCoinsInserted)) return false;
-    errorMessage = "";
-    return true;
-}
-
-function inStock() {
+function inStockAndEnoughInserted(valueOfCoinsInserted) {
     if (cokesInStore < 1) {
         errorMessage = "Det er ikke mer Cola igjen :(";
         updateView();
         return false;
     }
-    return true;
-}
-
-function hasInsertedEnough(valueOfCoinsInserted) {
     if (valueOfCoinsInserted < cokePrice) {
         errorMessage = "Du har ikke nok penger";
         updateView();
         return false;
     }
+    errorMessage = "";
     return true;
 }
-/*
-   1. Sjekke om du har nok penger (og få opp feilmelding hvis ikke)
-   2. Hvis du betaler for mye skal du få riktig veksel tilbake
-   3. Cola skal vises i utkast (utenfor blå boks) OK
-   4. Cola i maskin skal minke OK
-   5. Sjekke at det er cola i maskinen (få opp feilmelding hvis ikke) OK
-   6. Myntene du har betalt for cola skal inn i maskinen (coinsInMachine)
-*/
