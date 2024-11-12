9a851c44374f38a7a05758a6ad35aaafd66c840a
VadymMakohon <errorks7@gmail.com>
2024-11-12 11:36:27 +0100

    updated the script

--- Emne 1/Palindrom/script.js
+++ Emne 1/Palindrom/script.js
@@ -1,6 +1,11 @@
 const input = document.getElementById("input")
 
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+
 function check() {
     const value = input.value
-    alert(value)
+    const reverse = reverseString(value)
+    alert(reverse)
 }
