6f925d8b22d8d7521542b36e78dbfac5b387bed2
VadymMakohon <errorks7@gmail.com>
2024-11-13 09:56:20 +0100

    adding script

--- Emne 1/Stop Watch/script.js
+++ Emne 1/Stop Watch/script.js
@@ -2,13 +2,21 @@ let secondElapsed = 0;
 let interval = null;
 const time = documen.getElementById("time")
 
+function setTime() {
+    time.innerHTML = secondsElapsed;
+}
+
+function timer() {
+    secondsElapsed++;
+    setTime()
+}
 
 function startClock() {
-
+    secondsElapsed++;
 }
 
 function stopClock() {
-
+    interval = setInterval(timer, 1000)
 }
 
 function resetClock() {
