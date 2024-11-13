cf9659e055259d595f7252cc0618fd8fcba83c73
VadymMakohon <errorks7@gmail.com>
2024-11-13 09:24:05 +0100

    minor update

---
+++ Emne 1/Stop Watch/script.js
@@ -0,0 +1,16 @@
+let secondElapsed = 0;
+let interval = null;
+const time = documen.getElementById("time")
+
+
+function startClock() {
+
+}
+
+function stopClock() {
+
+}
+
+function resetClock() {
+
+}
