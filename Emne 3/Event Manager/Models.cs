8bbfa94ae584d5a63b81b0a98a32ddf6146e0ed5
VadymMakohon <errorks7@gmail.com>
2024-11-17 10:23:38 +0100

    adding new project

---
+++ Emne 3/Event Manager/Models.cs
@@ -0,0 +1,30 @@
+public class User : IdentityUser
+{
+    public string FullName { get; set; }
+    public ICollection<Registration> Registrations { get; set; }
+}
+
+public class Event
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DateTime { get; set; }
+    public string Location { get; set; }
+    public int Capacity { get; set; }
+    public int RegistrationsCount { get; set; }
+    public bool IsOpen { get; set; } = true;
+
+    public ICollection<Registration> Registrations { get; set; }
+}
+
+public class Registration
+{
+    public int Id { get; set; }
+    public int EventId { get; set; }
+    public string UserId { get; set; }
+    public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+    public Event Event { get; set; }
+    public User User { get; set; }
+}
