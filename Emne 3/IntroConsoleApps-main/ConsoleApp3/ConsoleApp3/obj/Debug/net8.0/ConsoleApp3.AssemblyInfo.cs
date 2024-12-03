dd307bb738ada5a827e8e4e6ade7f77d878bc4ca
VadymMakohon <errorks7@gmail.com>
2024-12-03 12:50:05 +0100

    added new file

--- Emne 3/IntroConsoleApps-main/ConsoleApp3/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ Emne 3/IntroConsoleApps-main/ConsoleApp3/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834381ffe6e659ae640e22697aba93988562c448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07c646ba9e94ce18b4835891453e1d1baf3d290")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
