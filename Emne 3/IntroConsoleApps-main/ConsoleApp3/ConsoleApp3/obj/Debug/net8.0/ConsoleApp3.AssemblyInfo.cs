7312e3b6311e8e95a6f161a31d4bf7efcfdcb390
VadymMakohon <errorks7@gmail.com>
2024-12-12 13:28:19 +0100

    updates

--- Emne 3/IntroConsoleApps-main/ConsoleApp3/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ Emne 3/IntroConsoleApps-main/ConsoleApp3/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd307bb738ada5a827e8e4e6ade7f77d878bc4ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c44087da836691294dc1265118d4e8c9bd40505")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
