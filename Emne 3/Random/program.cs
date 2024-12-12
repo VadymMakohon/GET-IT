c0844980ec0788075e5ed1def2a05a66e7738d65
VadymMakohon <errorks7@gmail.com>
2024-12-12 13:29:58 +0100

    updated code

--- Emne 3/Random/program.cs
+++ Emne 3/Random/program.cs
@@ -31,4 +31,35 @@ class Program
         Console.WriteLine("Bilen er fremme og har parkert.");
     }
 
-    
+    static void TwoCarRace()
+    {
+        Random random = new Random();
+        int car1Distance = 0, car2Distance = 0;
+        const int finishLine = 10000;
+
+        while (car1Distance < finishLine && car2Distance < finishLine)
+        {
+            int car1Speed = random.Next(60, 201);
+            int car2Speed = random.Next(60, 201);
+
+            car1Distance += car1Speed;
+            car2Distance += car2Speed;
+
+            Console.WriteLine($"Bil 1: {car1Distance} meter (Hastighet: {car1Speed} m/s)");
+            Console.WriteLine($"Bil 2: {car2Distance} meter (Hastighet: {car2Speed} m/s)\n");
+        }
+
+        if (car1Distance >= finishLine && car2Distance >= finishLine)
+        {
+            Console.WriteLine("Det ble uavgjort!");
+        }
+        else if (car1Distance >= finishLine)
+        {
+            Console.WriteLine("Bil 1 vant kappløpet!");
+        }
+        else
+        {
+            Console.WriteLine("Bil 2 vant kappløpet!");
+        }
+    }
+}
